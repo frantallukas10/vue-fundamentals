2b6a4f40dd8c1baa69a9f87e80e982ac54af2f05
FRANTL01 <lukas.frantal01@dixonscarphone.com>
2019-04-05 16:12:52 +0200

    init vue project

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # vue-fundamentals
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn run build
+```
+
+### Run your tests
+
+```
+yarn run test
+```
+
+### Lints and fixes files
+
+```
+yarn run lint
+```
+
+### Run your unit tests
+
+```
+yarn run test:unit
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### pohlad na Vue
+
+1. naucit sa pouzivat snippet
+   https://vuejs.github.io/vetur/snippet.html#scaffold-snippets
+
+2. lang definujem typ zapisu styles a scoped je atribut ktorim definujeme ze style nie je dostupny pre ine kopmonenty
+
+```html
+<style lang="sass" scoped></style>
+```
+
+3.
---
+++ src/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <h1 class="h1">Work time log</h1>
+    <img class="mb-2 img-fluid" src="@/assets/home.jpg" alt="logo" />
+    <b-form @submit.prevent="onSubmit" @reset.prevent="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Osobne číslo"
+        label-for="input-1"
+        description="napr. z prizviska 5 znakov plus prve pismeno z mena + 01 ak si neexistuje niekto z rovnakym zaciatkom"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.accountName"
+          type="number"
+          required
+          placeholder="Napiš osobne čislo zo zmluvy"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-2"
+        label="Heslo"
+        label-for="input-2"
+        description="heslo rovnake ako prihlasovacie do tvojho PC"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.pass"
+          type="password"
+          required
+          placeholder="Napiš heslo do PC"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button class="mr-2" type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+export default class Home extends Vue {
+  form = {
+    accountName: '',
+    pass: ''
+  }
+  show = true
+
+  onSubmit() {
+    console.log(this.form.accountName, this.form.pass)
+  }
+
+  onReset() {
+    this.form.accountName = ''
+    this.form.pass = ''
+    this.show = false
+
+    this.$nextTick(() => {
+      this.show = true
+    })
+  }
+}
+</script>
+
